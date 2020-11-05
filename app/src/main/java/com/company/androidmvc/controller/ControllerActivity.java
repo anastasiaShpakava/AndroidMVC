ec23c4cf6b5d3d06ec4c13f09cdcb370db6f2009
anastasiaShpakava <shpakavaa@gmail.com>
2020-11-05 21:09:24 +0300

    created controller class
    (extends Application to has global access)

---
+++ app/src/main/java/com/company/androidmvc/controller/ControllerActivity.java
@@ -0,0 +1,31 @@
+package com.company.androidmvc.controller;
+
+
+import android.app.Application;
+
+import com.company.androidmvc.model.Cart;
+import com.company.androidmvc.model.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ControllerActivity extends Application { //для глобальн. доступа
+    private List<Product> myProduct = new ArrayList<>();
+    private Cart cart = new Cart();
+
+    public Product getProduct(int position) {
+        return myProduct.get(position);
+    }
+
+    public void setProduct(Product product) {
+        myProduct.add(product);
+    }
+
+    public Cart getCart() {
+        return cart;
+    }
+
+    public int getProductArrayListSize() {
+        return myProduct.size();
+    }
+}
